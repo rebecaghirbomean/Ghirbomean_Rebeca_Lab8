625a15464ed84bdad381cb732e2fa8941b352148
rebecaghirbomean <92678752+rebecaghirbomean@users.noreply.github.com>
2022-01-13 01:39:31 +0200

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace Ghirbomean_Rebeca_Lab8.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        [Required, StringLength(150, MinimumLength = 3)]
+        [Display(Name = "Book Title")]
+        public string Title { get; set; }
+        [RegularExpression(@"^[A-Z][a-z]+\s[A-Z][a-z]+$", ErrorMessage = "Numele autorului trebuie sa fie de forma 'Prenume Nume"), Required, StringLength(50, MinimumLength = 3)]
+        public string Author { get; set; }
+        [Range(1, 300)]
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
+        public int PublisherID { get; set; }
+        public Publisher Publisher { get; set; }
+        public ICollection<BookCategory> BookCategories { get; set; }
+    }
+}
